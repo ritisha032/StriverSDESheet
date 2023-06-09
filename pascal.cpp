7e818beab7d2d71a6240ed7600f5b181099c0f76
ritisha032 <ritishasingh74@gmail.com>
2023-06-09 11:46:15 +0530

    Create pascal.cpp

---
+++ pascal.cpp
@@ -0,0 +1,27 @@
+#include <vector.h>
+
+vector<vector<long long int>> printPascal(int n) 
+{
+  // Write your code here.
+  vector<vector<long long int>> ans;
+
+  for(int i=0;i<n;++i){
+
+      vector<long long int> v;
+
+
+      for(int k=0;k<=i;++k){
+
+          if(k==0 || k==i )
+             v.push_back(1);
+          else{
+
+            v.push_back(ans[i-1][k]+ans[i-1][k-1]);
+          }
+      }
+
+      ans.push_back(v);
+  }
+
+  return ans;
+}
