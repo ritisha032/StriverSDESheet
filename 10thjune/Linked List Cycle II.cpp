e05db52e981294a0da8431a28e8cd496ee4f4f77
ritisha032 <ritishasingh74@gmail.com>
2023-06-10 08:30:22 +0530

    10thjunefirstcommit

---
+++ 10thjune/Linked List Cycle II.cpp
@@ -0,0 +1,44 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+Node *firstNode(Node *head)
+{
+    //    Write your code here.
+    if(!head)
+        return head;
+    while(head){
+
+        if(head->data == -1)
+            return head;
+        head->data=-1;
+        head=head->next;
+    }
+
+    return NULL;
+}
